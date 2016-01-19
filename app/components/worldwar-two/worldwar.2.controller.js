4123247cf7a6674956d80a6fbbe012b0dccf1085
mgreen <martin.green@bwinparty.com>
2016-01-19 14:45:10 +0000

    directives

--- app/components/worldwar-two/worldwar.2.controller.js
+++ app/components/worldwar-two/worldwar.2.controller.js
@@ -8,9 +8,11 @@
   ww2.$inject = ['$scope'];
 
   function ww2($scope) {
-    $scope.firstName = "Johns";
+    $scope.firstName = "dave";
     $scope.lastName = "Doe";
-
+    $scope.dave = { name:'Dave', hair: 'ginger', sex: 'male' };
+    $scope.hannah = { name: 'Hannah', hair: 'blonde', sex: 'female' };
+    $scope.color = 'green';
   };
 
 })();
--- app/components/worldwar-two/worldwar.2.directive.js
+++ app/components/worldwar-two/worldwar.2.directive.js
@@ -13,6 +13,70 @@
       return {
         templateUrl: "./components/worldwar-two/inputs.html"
       }
+    })
+
+    .directive('ww2b', function() {
+      return {
+        restrict: 'E',
+        scope: {
+          user: '=user'
+        },
+        template: "<h2 ng-switch='user.sex'><span ng-switch-when='male'>His</span><span ng-switch-when='female'>Her</span> name is {{ user.name }}</h2>"
+      }
+    })
+
+    .directive('ww2c', function() {
+      return {
+        restrict: 'E',
+        scope: {
+          user: '@user'
+        },
+        template: "<h2>hi my name is {{ user.name }}</h2>"
+      }
+    })
+
+    .directive('helloWorld', function() {
+      return {
+        scope: {
+          color: '@colorAttr'
+        },
+        restrict: 'AE',
+        replace: true,
+        template: '<p style="background-color:{{color}}">Hello world',
+        link: function(scope, elem, attrs) {
+          elem.bind('click', function() {
+            elem.css('backgroung-color', 'white');
+            scope.$apply(function() {
+              scope.color = 'white';
+            });
+          });
+          elem.bind('mouseover', function() {
+            elem.css('cursor', 'pointer');
+          })
+        }
+      }
+    })
+
+    .directive('jelloWorlds', function() {
+      return {
+        scope: {
+          color: '@colorAttr'
+        },
+        restrict: 'AE',
+        replace: true,
+        template: '<p style="background-color:{{color}}">jello worlds',
+        link: function(scope, elem, attrs) {
+          elem.bind('click', function() {
+            elem.css('backgroung-color', 'white');
+            scope.$apply(function() {
+              scope.color = 'white';
+            });
+          });
+          elem.bind('mouseover', function() {
+            elem.css('cursor', 'pointer');
+          })
+        }
+      }
     });
 
 })();
